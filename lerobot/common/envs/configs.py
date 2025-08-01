ae7499c347ea4880660d207d42942ed041069d07
Shawn Wang <swang@tactasystems.com>
2025-08-01 16:00:11 -0700

    fix: update dataset

--- lerobot/common/envs/configs.py
+++ lerobot/common/envs/configs.py
@@ -264,7 +264,7 @@ class HILEnvConfig(EnvConfig):
     number_of_steps_after_success: int = 0
     ################# args from tacta
     mask_tactile: bool = False
-    mask_image_keys: list[str] = field(default_factory=lambda: ["front"])
+    mask_image_keys: list[str] = field(default_factory=lambda: [])
     ############################
 
     # 
