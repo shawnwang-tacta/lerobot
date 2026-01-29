bb95f05e5ccc58414bb0faa95874f97c32862e46
Shawn Wang <swang@tactasystems.com>
2026-01-28 18:31:34 -0800

    feat: deadband

--- lerobot/scripts/rl/action_provider_wrapper.py
+++ lerobot/scripts/rl/action_provider_wrapper.py
@@ -1,5 +1,5 @@
 def make_il_action_provider(model_server_url: str):
     from tacta.control.gym_env.flexiv_env.action_provider import ModelInferenceProviderConfig, ModelInferenceProvider
-    action_transform = {"type": "snap_joints", "enabled": True}
+    action_transform = {"type": "deadband", "enabled": True}
     provider_cfg = ModelInferenceProviderConfig(model_server_url=model_server_url, action_transform=action_transform)
     return ModelInferenceProvider(provider_cfg)
